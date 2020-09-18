2ed21169900f8d9ffc2430744a789edffe949f6b
TurBoss <j.l.toledano.l@gmail.com>
2020-09-18 04:24:42 +0200

    add hal pins

--- qtpyvcp_additive_manufacturing/svg_slicer/svg_slicer.py
+++ qtpyvcp_additive_manufacturing/svg_slicer/svg_slicer.py
@@ -1,14 +1,15 @@
+import os
 import sys
+
 from xml.dom import minidom
 
 from qtpy.QtCore import Slot
 from qtpy.QtWidgets import QApplication, QWidget, QVBoxLayout
 from qtpy.QtSvg import QSvgWidget, QSvgRenderer
 
-
-import os
-
-from qtpyvcp.plugins import getPlugin
+from qtpyvcp import hal
+from qtpyvcp.widgets import HALWidget
+from qtpyvcp.plugins import getPlugin, LOG
 
 STATUS = getPlugin('status')
 
@@ -16,24 +17,67 @@ STATUS = getPlugin('status')
 IN_DESIGNER = os.getenv('DESIGNER', False)
 
 
-class SvgSlicer(QSvgWidget):
+class SvgSlicer(QSvgWidget, HALWidget):
+    """HAL Connections for SvgSlicer
+
+    SvgSlicer will display layers according to `u32` and `s32` HAL pin values.
+
+    .. table:: HAL Pins
+
+        =============================== ========= =========
+        HAL Pin Name                    Type      Direction
+        =============================== ========= =========
+        qtpyvcp.svgslicer.enable        bool      in
+        qtpyvcp.svgslicer.layer.in      s32       in
+        qtpyvcp.svgslicer.layer.count   s32       out
+        =============================== ========= =========
+
+    Notes:
+        ...
+    """
 
     def __init__(self, parent=None):
         super(SvgSlicer, self).__init__(parent)
 
         self.status = STATUS
 
+        self._enabled_pin = None
+        self._layer_in_pin = None
+        self._layer_count_pin = None
+
         self._svg_header = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
 <svg width="23.838" height="23.838" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:slic3r="http://slic3r.org/namespaces/slic3r">
 """
         self._svg_end = "</svg>"
+
         self._current_layer = 0
+        self._num_layers = 0
 
         self._file_name = ""
         self._layers = None
 
         self.status.file.notify(self.load)
+
+    def initialize(self):
+        comp = hal.getComponent()
+        obj_name = self.getPinBaseName()
+
+        # enable HAL pin
+        _pin_name = ".".join([obj_name, "enable"])
+        self._enabled_pin = comp.addPin(_pin_name, "bit", "in")
+        self._enabled_pin.value = self.isEnabled()
+        self._enabled_pin.valueChanged.connect(self.setEnabled)
+
+        # layer in HAL pin
+        _pin_name = ".".join([obj_name, "layer", "in"])
+        self._layer_in_pin = comp.addPin(_pin_name, "s32", "in")
+        self._layer_in_pin.valueChanged.connect(self.selectLayer)
+
+        # layer count out HAL pin
+        _pin_name = ".".join([obj_name, "layer", "count"])
+        self._layer_count_pin = comp.addPin(_pin_name, "s32", "out")
+        self._layer_count_pin.value = self._num_layers
 
     def get_layers(self):
         xmldoc = minidom.parse(self._file_name)
@@ -71,10 +115,11 @@ class SvgSlicer(QSvgWidget):
     def load(self, file_name):
         self._file_name = file_name
         self._layers = self.get_layers()
+        self._num_layers = len(self._layers)
+        self._layer_count_pin.value = self._num_layers
 
         self.draw()
 
-    @Slot(int)
     def selectLayer(self, layer):
         self._current_layer = layer
 
